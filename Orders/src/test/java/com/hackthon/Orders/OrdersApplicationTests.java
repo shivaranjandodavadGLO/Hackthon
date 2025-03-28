f49f07e12e29da5327f9c40b63c66caa50990873
shivaranjan.dodavad <shivaranjan.dodavad@globallogic.com>
2025-03-28 12:44:05 +0530

    Orders module created
    Signed-off-by: shivaranjan.dodavad <shivaranjan.dodavad@globallogic.com>

---
+++ Orders/src/test/java/com/hackthon/Orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.Orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
